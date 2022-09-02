d5dac343ef57cfc06ad35f114d71e872009ff3bb
Cihad Atınç <atinccihad@gmail.com>
2022-09-02 23:20:54 +0300

    jdbc

---
+++ src/main/java/jdbc/Main.java
@@ -0,0 +1,18 @@
+package jdbc;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // DBWork objesini olustur.
+        DBWork db = new DBWork();
+
+        // Connection methodunu databaseden cagirdik.
+        Connection con = db.connect_to_db("techproed", "postgres", "atinccihadGmail");
+
+        // Yeni table olusturma methodunu cagir.
+        db.createTable(con,"employees");
+
+    }
+}
