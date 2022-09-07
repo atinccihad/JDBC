86de85efb1949a82213281e6228831bf56560f48
Cihad Atınç <atinccihad@gmail.com>
2022-09-07 17:55:40 +0300

    jdbc

---
+++ src/main/java/jdbcPractice/Query04.java
@@ -0,0 +1,27 @@
+package jdbcPractice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Query04 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed",
+                "postgres",
+                "atinccihadGmail");
+
+        PreparedStatement ps = con.prepareStatement("insert into ogrenciler values(?,?,?,?)");
+        ps.setInt(1,200);
+        ps.setString(2,"Veli Can");
+        ps.setString(3,"12");
+        ps.setString(4,"E");
+
+        ps.executeUpdate();
+
+        System.out.println("Veri girisi yaoildi.");
+
+    }
+}
